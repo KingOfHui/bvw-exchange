3f583fdfc1d36544fb09a249c576e3c4b9b0479e
dinghui <dinghui@bcbook.com>
2020-09-08 17:11:43 +0800

    add:添加硬件矿机UI页面

---
+++ app/src/main/java/com/darknet/bvw/activity/HardwareMineralActivity.java
@@ -0,0 +1,26 @@
+package com.darknet.bvw.activity;
+
+import com.darknet.bvw.R;
+
+/**
+ * @ClassName HardwareMineralActivity
+ * @Description
+ * @Author dinghui
+ * @Date 2020/9/8 0008 11:36
+ */
+public class HardwareMineralActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_hard_mineral;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initDatas() {
+
+    }
+}
---
+++ app/src/main/java/com/darknet/bvw/activity/MineralInfoActivity.java
@@ -0,0 +1,26 @@
+package com.darknet.bvw.activity;
+
+import com.darknet.bvw.R;
+
+/**
+ * @ClassName MineralInfoActivity
+ * @Description
+ * @Author dinghui
+ * @Date 2020/9/8 0008 16:02
+ */
+public class MineralInfoActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_mineral_info;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initDatas() {
+
+    }
+}
