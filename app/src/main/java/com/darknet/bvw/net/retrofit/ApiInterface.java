be990167229a8e563db1f3455405fed20438cf6a
Eshel <eshel@haha.com>
2020-12-21 17:39:35 +0800

    写崩了, 啥数据都没了

--- app/src/main/java/com/darknet/bvw/net/retrofit/ApiInterface.java
+++ app/src/main/java/com/darknet/bvw/net/retrofit/ApiInterface.java
@@ -14,8 +14,6 @@ import io.reactivex.Observable;
 import okhttp3.RequestBody;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
-import retrofit2.http.Query;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
