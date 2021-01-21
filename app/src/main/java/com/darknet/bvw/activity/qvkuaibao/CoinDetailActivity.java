08b9b789ae19e47f067c535e142040824e88657e
dinghui <dinghui@bcbook.com>
2021-01-21 20:03:43 +0800

    货币详情界面UI

---
+++ app/src/main/java/com/darknet/bvw/activity/qvkuaibao/CoinDetailActivity.java
@@ -0,0 +1,32 @@
+package com.darknet.bvw.activity.qvkuaibao;
+
+import androidx.databinding.ViewDataBinding;
+
+import com.darknet.bvw.R;
+import com.darknet.bvw.activity.BaseBindingActivity;
+
+/**
+ * <pre>
+ *     author : dinghui
+ *     e-mail : dinghui@bcbook.com
+ *     time   : 2021/01/21
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class CoinDetailActivity extends BaseBindingActivity<ViewDataBinding> {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_coin_detail;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initDatas() {
+
+    }
+}
--- app/src/main/java/com/darknet/bvw/activity/qvkuaibao/QvKuaiBaoCoinListActivity.java
+++ app/src/main/java/com/darknet/bvw/activity/qvkuaibao/QvKuaiBaoCoinListActivity.java
@@ -1,8 +1,9 @@
-package com.darknet.bvw.activity;
+package com.darknet.bvw.activity.qvkuaibao;
 
 import androidx.databinding.ViewDataBinding;
 
 import com.darknet.bvw.R;
+import com.darknet.bvw.activity.BaseBindingActivity;
 
 public class QvKuaiBaoCoinListActivity extends BaseBindingActivity<ViewDataBinding> {
     @Override
